7be574faf1aad95962253efbebbdf5e8c384d9fa
Deser7 <deser7@icloud.com>
2025-08-27 19:36:12 +0400

    создал новый проект

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Наташа Спиридонова on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
